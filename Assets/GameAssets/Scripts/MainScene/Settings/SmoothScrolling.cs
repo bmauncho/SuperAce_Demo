20ca40bd8310573dffb959de3836631245d3262c
BradleyMauncho <bmauncho@gmail.com>
2025-01-17 04:04:46 +0300

    update

--- Assets/GameAssets/Scripts/MainScene/Settings/SmoothScrolling.cs
+++ Assets/GameAssets/Scripts/MainScene/Settings/SmoothScrolling.cs
@@ -5,21 +5,52 @@ public class SmoothScrolling : MonoBehaviour
 {
     [Tooltip("Smoothness of the scrolling")]
     public float smoothTime = 0.1f;
-
+    private float currentVelocity = 0f; // For smooth scrolling
+    private float targetScrollPosition;
     private ScrollRect scrollRect;
     public RectTransform slidingArea;
     public RectTransform handle;
 
     private Vector2 velocity;
 
+    public bool isScrolling =false;
+
     void Start ()
     {
         // Get the ScrollRect component attached to this GameObject
         scrollRect = GetComponent<ScrollRect>();
+        scrollRect.verticalNormalizedPosition = 1;
     }
 
     void Update ()
     {
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scrollInput != 0)
+        {
+            isScrolling = true;
+            scrollRect.enabled = false;
+            // Adjust the target scroll position based on input
+            targetScrollPosition += scrollInput * smoothTime;
+            
+            // Clamp the target position between 0 and 1
+            targetScrollPosition = Mathf.Clamp01(targetScrollPosition);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            isScrolling = false;
+            scrollRect.enabled = true;
+        }
+
+        if(isScrolling)
+        {
+            scrollRect.verticalNormalizedPosition = Mathf.SmoothDamp(
+            scrollRect.verticalNormalizedPosition ,
+            targetScrollPosition ,
+            ref currentVelocity ,
+            smoothTime);
+        }
         // Get the height of the SlidingArea
         float slidingAreaHeight = slidingArea.rect.height;
 
